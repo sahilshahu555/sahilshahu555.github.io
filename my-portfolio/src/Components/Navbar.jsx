653781f4b4a58ba1c7f23069a338fdf1f91ada66
Sahil Shahu <sahilshahu555@gmail.com>
2023-06-21 13:52:49 +0530

    ee

--- my-portfolio/src/Components/Navbar.jsx
+++ my-portfolio/src/Components/Navbar.jsx
@@ -10,12 +10,12 @@ const Navbar = () => {
       
         
        <ul >
-        <li className="item nav-link home" ><a href="#home" className="item-a " >Home</a></li>
-        <li className="item nav-link about" ><a href="#about"  className="item-a"  >About</a></li>
-        <li className="item nav-link skills" ><a href="#skills" className="item-a"  >Skills</a></li>
-        <li className="item nav-link home projects" ><a href="#projects" className="item-a" >Projects</a></li>
-        <li className="item nav-link contact" ><a href="#contact" className="item-a"  >Contact </a></li>
-        <li className="item" ><a href="#contact" className="item-a nav-link resume"  >Resume</a></li>
+          <a href="#home" className="item-a nav-link home" >Home</a> 
+          <a href="#about"  className="item-a nav-link about">About</a>
+          <a href="#skills" className="item-a nav-link skills"  >Skills</a>
+          <a href="#projects" className="item-a nav-link projects" >Projects</a>
+          <a href="#contact" className="item-a nav-link contact" >Contact </a>
+          <a href="#contact" className="item-a nav-link resume"  >Resume</a>
       </ul>
     </div>
   )
