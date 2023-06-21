f73efc36363a784926dbe9edd60472c1e2ed9006
Sahil Shahu <sahilshahu555@gmail.com>
2023-06-21 13:28:39 +0530

    add class names to navbar elements

--- my-portfolio/src/Components/Navbar.jsx
+++ my-portfolio/src/Components/Navbar.jsx
@@ -11,11 +11,11 @@ const Navbar = () => {
         
        <ul >
         <li className="item nav-link home" ><a href="#home" className="item-a " >Home</a></li>
-        <li className="item" ><a href="#about"  className="item-a"  >About</a></li>
-        <li className="item" ><a href="#skills" className="item-a"  >Skills</a></li>
-        <li className="item" ><a href="#projects" className="item-a" >Projects</a></li>
-        <li className="item" ><a href="#contact" className="item-a"  >Contact </a></li>
-        <li className="item" ><a href="#contact" className="item-a"  >Resume</a></li>
+        <li className="item nav-link about" ><a href="#about"  className="item-a"  >About</a></li>
+        <li className="item nav-link skills" ><a href="#skills" className="item-a"  >Skills</a></li>
+        <li className="item nav-link home projects" ><a href="#projects" className="item-a" >Projects</a></li>
+        <li className="item nav-link contact" ><a href="#contact" className="item-a"  >Contact </a></li>
+        <li className="item" ><a href="#contact" className="item-a nav-link resume"  >Resume</a></li>
       </ul>
     </div>
   )
