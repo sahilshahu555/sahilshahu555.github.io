1ee0d99e1ad3c7ed335b88e38756c978355e5fb6
Sahil Shahu <sahilshahu555@gmail.com>
2023-06-21 11:58:10 +0530

    aa

--- my-portfolio/src/Components/Navbar.jsx
+++ my-portfolio/src/Components/Navbar.jsx
@@ -2,49 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div id="nav-menu" style={{display:"flex",justifyContent:"space-around",
-    alignItems:"center",
-    backgroundColor:"lightblue",
-    }}>
+    <div id="nav-menu" >
         <div><h1>Sahil Shahu</h1></div>
-       <ul style={{display:"grid",gridTemplateColumns:"repeat(5,1fr)", gap:"50px",
-    }}>
-        <li class="item" style={{listStyle: "none"}}><a href="#home" style={{textDecoration:"none",
-         color:"black",
-         fontWeight:"bold"
-        }} 
-    >Home</a></li>
-        <li class="item"
-        style={{listStyle: "none"}}
-        ><a href="#about"
-        style={{textDecoration:"none",
-        color:"black",
-        fontWeight:"bold"
-       }}
-        >About</a></li>
-        <li class="item"
-         style={{listStyle: "none"}}><a href="#skills"
-         style={{textDecoration:"none",
-        color:"black",
-        fontWeight:"bold"
-       }}
-         >Skills</a></li>
-        <li class="item"
-         style={{listStyle: "none"}}
-        ><a href="#projects"
-        style={{textDecoration:"none",
-        color:"black",
-        fontWeight:"bold"
-       }}
-        >Projects</a></li>
-        <li class="item" 
-         style={{listStyle: "none"}}
-        ><a href="#contact"
-        style={{textDecoration:"none",
-        color:"black",
-        fontWeight:"bold"
-       }}
-        >Contact </a></li>
+       <ul >
+        <li class="item " ><a href="#home"   class="item-a nav-link-home" >Home</a></li>
+        <li class="item" ><a href="#about"  class="item-a"  >About</a></li>
+        <li class="item" ><a href="#skills" class="item-a"  >Skills</a></li>
+        <li class="item" ><a href="#projects" class="item-a" >Projects</a></li>
+        <li class="item" ><a href="#contact" class="item-a"  >Contact </a></li>
       </ul>
     </div>
   )
