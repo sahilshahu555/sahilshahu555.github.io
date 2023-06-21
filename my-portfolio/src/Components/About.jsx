5b6a47a5547c86c5ee7a896e0f535c207adebea7
Sahil Shahu <sahilshahu555@gmail.com>
2023-06-21 17:47:52 +0530

    tt

--- my-portfolio/src/Components/About.jsx
+++ my-portfolio/src/Components/About.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const About = () => {
   return (
     <div id="about" className="about section">
-      <h1 id="user-detail-intro">About Me</h1>
+      <h1>About Me</h1>
+      <p id="user-detail-intro">An enthusiastic Full-stack Developer, skilled in Node.JS. Experienced in Developing websites using JavaScript, Node.js, Express.js, MongoDB. An optimistic person, always ready to take critical decisions and ready to work under pressure. Always ready to try some new things for optimization.</p>
 
       
     
--- my-portfolio/src/Components/Home.jsx
+++ my-portfolio/src/Components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <div id='home'>
     <img src="" alt="" srcset="" className="home-img"/>
     <h2 id="user-detail-name">Hello, I am Sahil Shahu</h2> 
-    <p id="user-detail-intro">An enthusiastic Full-stack Developer, skilled in Node.JS. Experienced in Developing websites using JavaScript, Node.js, Express.js, MongoDB. An optimistic person, always ready to take critical decisions and ready to work under pressure. Always ready to try some new things for optimization.</p>
+    
      <a id="resume-link-2" href="/#"> <button  id="resume-button-2"> Resume</button></a>
     </div>
   )
