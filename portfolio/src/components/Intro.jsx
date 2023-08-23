c439498cdd912bcfae344c64fe5c05664d2862b3
Poojasharma111 <ps1610572@gmail.com>
2023-08-23 22:23:23 +0530

    edited resume and intro section

--- portfolio/src/components/Intro.jsx
+++ portfolio/src/components/Intro.jsx
@@ -35,7 +35,7 @@ export const Intro = () => {
 
                     <Text data-aos="zoom-out-up" data-aos-duration="1000" mt={"30px"} >
                         <Button size={["sm", "md", "md", "md"]} id="resume-button-2" onClick={() => handleclick(resume)}
-                            href="https://drive.google.com/file/d/1a7tbFkpsUcxQPwzOoCL-TGOUXm30ctFO/view?usp=sharing" as={Link}
+                            href="https://drive.google.com/file/d/1c5tn3oRihetBMKptsGGx9Gk0hqRoZfUx/view?usp=sharing" as={Link}
                             target="_blank" download={true}
                             color={"white"}
 
--- portfolio/src/components/Nav.jsx
+++ portfolio/src/components/Nav.jsx
@@ -79,7 +79,7 @@ export const Nav = () => {
         <Button
         id="resume-button-1"
           display={["none", "none", "none", "flex", "flex"]} borderRadius=".8rem" size={["sm", "md"]} margin="auto" as={Link} border="1px solid #e4512d"
-          target="_blank" bg="brand_secondary" color="#f8572f" _hover={{bg: "gray.200", color:"#e4512d" }} href="https://drive.google.com/file/d/1a7tbFkpsUcxQPwzOoCL-TGOUXm30ctFO/view?usp=sharing" download={true}
+          target="_blank" bg="brand_secondary" color="#f8572f" _hover={{bg: "gray.200", color:"#e4512d" }} href="https://drive.google.com/file/d/1c5tn3oRihetBMKptsGGx9Gk0hqRoZfUx/view?usp=sharing" download={true}
           onClick={()=>handleClick(resume)}
          
         >
@@ -226,7 +226,7 @@ export const Nav = () => {
             color="#f8572f"
             border="1px"
             borderRadius="10px"
-            href="https://drive.google.com/file/d/1a7tbFkpsUcxQPwzOoCL-TGOUXm30ctFO/view?usp=sharing"
+            href="https://drive.google.com/file/d/1c5tn3oRihetBMKptsGGx9Gk0hqRoZfUx/view?usp=sharing"
             download={true}
             onClick={()=>handleClick(resume)}
             id="resume-button-1"
